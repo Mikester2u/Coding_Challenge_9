80f0a2dbdd3e806da581901cc6d97783fb61a671
Michael Rueda <michael.a.rueda22@gmail.com>
2024-06-14 13:41:04 -0400

    implement interactive behaviors with D3.js

---
+++ script.js
@@ -0,0 +1,18 @@
+//U22562170
+function createInteractiveDivs() {
+    const data = [1, 2, 3, 4, 5];
+    const container = d3.select('body').append('div').attr('class', 'container');
+
+    container.selectAll(.data-point)
+    .data(data)
+    .enter()
+    .append('div');
+    .attr('class', 'data-point')
+    .style('background-color', 'greenyellow')
+    .style('width', '100px')
+    .style('height', '100px')
+    .style('margin', '10px')
+    .style('display', 'inline-block')
+    .on('mouseover', handleMouseOver)
+    .on('mouseout', handleMouseOut);
+}
